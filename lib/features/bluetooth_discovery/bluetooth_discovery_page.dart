c7a28451d7d28d98be3caddbbecfeb6e304f4904
SachaBarbet <sacha.barbet@proton.me>
2025-02-04 17:41:02 +0100

    Clean init project; Create BluetoothDiscoveryPage

---
+++ lib/features/bluetooth_discovery/bluetooth_discovery_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BluetoothDiscoveryPage extends StatelessWidget {
+  const BluetoothDiscoveryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
