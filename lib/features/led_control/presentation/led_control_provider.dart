44d4867ac2aed07e1314a9c0b9fba1d9f1fb6a09
Sacha <117525182+SachaBarbet@users.noreply.github.com>
2025-02-08 18:23:03 +0100

    Add led control page and trigger / sub to topics

--- lib/features/led_control/presentation/led_control_provider.dart
+++ lib/features/led_control/presentation/led_control_provider.dart
@@ -1,24 +1,88 @@
+import 'dart:convert';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mqtt_client/mqtt_client.dart';
 
 import '../../../shared/mqtt/data/esp_mode_enum.dart';
+import '../../../shared/mqtt/presentation/mqtt_service_provider.dart';
 import '../data/led_control_entity.dart';
+import '../data/led_topic_constants.dart';
 
-final ledControlProvider =
-StateNotifierProvider<LedControlNotifier, LedControl>((ref) {
+final ledControlProvider = StateNotifierProvider<LedControlNotifier, LedControl>((ref) {
   return LedControlNotifier(ref);
 });
 
 class LedControlNotifier extends StateNotifier<LedControl> {
   final Ref ref;
 
-  LedControlNotifier(this.ref) : super(LedControl(
+  LedControlNotifier(this.ref) : super(const LedControl(
     isSubscribed: false,
     lastUpdateAt: null,
-    espMode: EspMode.auto,
+    espMode: EspMode.manual, // Default mode
     ledValues: [0, 0, 0],
     luxValue: 0,
-    luxTriggerOn: 0,
-    luxTriggerOff: 0,
+    luxTriggerOn: 100,
+    luxTriggerOff: 50,
     luxDebounced: false,
   ));
+
+  void updateState(LedControl newState) {
+    state = newState;
+  }
+
+  void subscribeToLedState() {
+    ref.read(mqttServiceProvider.notifier).client.subscribe(ledDataTopic, MqttQos.atLeastOnce);
+
+    ref.read(mqttServiceProvider.notifier).client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? messages) {
+      final recMessage = messages![0].payload as MqttPublishMessage;
+      final payload = MqttPublishPayload.bytesToStringAsString(recMessage.payload.message);
+
+      _handleLedStateUpdate(payload);
+
+    });
+  }
+
+  void _handleLedStateUpdate(String payload) {
+    print('Received message: $payload');
+    final Map<String, dynamic> data = jsonDecode(payload);
+
+    final ledControl = LedControl(
+      isSubscribed: true,
+      lastUpdateAt: DateTime.now(),
+      espMode: EspMode.values[data['espMode']] ?? EspMode.manual,
+      ledValues: List<int>.from(data['ledValues']) ?? [0, 0, 0],
+      luxValue: data['luxValue'] ?? 0,
+      luxTriggerOn: data['luxTriggerOn'] ?? 100,
+      luxTriggerOff: data['luxTriggerOff'] ?? 50,
+      luxDebounced: data['luxDebounced'] ?? false,
+    );
+
+    // Update state
+    ref.read(ledControlProvider.notifier).updateState(ledControl);
+  }
+
+  void requestLedState() {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString('ping');
+
+    ref.read(mqttServiceProvider.notifier).client.publishMessage(ledPingTopic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  void setLedValues(List<int> values) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(values.join(','));
+
+    ref.read(mqttServiceProvider.notifier).client.publishMessage(ledControlTopic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  void setControlMode(EspMode mode) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(mode.index.toString());
+
+    ref.read(mqttServiceProvider.notifier).client.publishMessage(ledModeTopic, MqttQos.atLeastOnce, builder.payload!);
+  }
+
+  void unsubscribe() {
+    ref.read(mqttServiceProvider.notifier).client.unsubscribe(ledDataTopic);
+  }
 }
--- lib/features/led_control/presentation/led_control_widget.dart
+++ lib/features/led_control/presentation/led_control_widget.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../shared/mqtt/data/esp_mode_enum.dart';
+import 'led_control_provider.dart';
 
 class LedControlWidget extends ConsumerStatefulWidget {
   const LedControlWidget({super.key});
@@ -9,23 +13,133 @@ class LedControlWidget extends ConsumerStatefulWidget {
 }
 
 class _LedControlState extends ConsumerState<LedControlWidget> {
-
   @override
   void initState() {
-    // TODO: subscribe to data topic and publish to ping topic
+    final LedControlNotifier notifier = ref.read(ledControlProvider.notifier);
+    notifier.subscribeToLedState();
+    notifier.requestLedState();
     super.initState();
   }
 
   @override
   void dispose() {
-    // TODO: unsubscribe data led esp
     super.dispose();
+  }
+
+  void _showColorPicker(BuildContext context, Color selectedColor) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text("Pick a Color"),
+        content: SingleChildScrollView(
+          child: MaterialPicker(
+            pickerColor: selectedColor,
+            onColorChanged: (color) {
+              selectedColor = color;
+            },
+            enableLabel: true,
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context); // Close dialog
+            },
+            child: const Text("Cancel"),
+          ),
+          TextButton(
+            onPressed: () {
+              _sendColorToMqtt(selectedColor);
+              Navigator.pop(context);
+            },
+            child: const Text("Set Color"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _sendColorToMqtt(Color color) {
+    // Convert color to RGB values
+    List<int> rgbValues = [color.r.toInt(), color.g.toInt(), color.b.toInt()];
+    ref.read(ledControlProvider.notifier).setLedValues(rgbValues);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    final ledState = ref.watch(ledControlProvider);
+    final notifier = ref.watch(ledControlProvider.notifier);
 
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Center(
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Control mode: "),
+                Row(
+                  children: [
+                    Text("Auto"),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Switch(
+                        value: ledState.espMode == EspMode.manual,
+                        onChanged: (value) => notifier.setControlMode(
+                          value ? EspMode.auto : EspMode.manual,
+                        ),
+                      ),
+                    ),
+                    Text("Manual")
+                  ],
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: Divider(),
+            ),
+            // Led icon, color picker
+            // Values not editable : Lux value, trigger on, trigger off, debounced
+            Icon(
+              Icons.lightbulb,
+              size: 100,
+              color: Color.fromRGBO(
+                ledState.ledValues[0],
+                ledState.ledValues[1],
+                ledState.ledValues[2],
+                1,
+              ),
+            ),
+            Column(
+              children: [
+                Text("RGB: ${ledState.ledValues}"),
+                ElevatedButton(
+                  onPressed: () => _showColorPicker(
+                    context,
+                    Color.fromRGBO(
+                      ledState.ledValues[0],
+                      ledState.ledValues[1],
+                      ledState.ledValues[2],
+                      1,
+                    ),
+                  ),
+                  child: const Text("Change color"),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 24),
+              child: Divider(),
+            ),
+            Text("Lux value: ${ledState.luxValue}"),
+            Text("Trigger on: ${ledState.luxTriggerOn}"),
+            Text("Trigger off: ${ledState.luxTriggerOff}"),
+            Text("Debounced: ${ledState.luxDebounced}"),
+          ],
+        ),
+      ),
     );
   }
 }
