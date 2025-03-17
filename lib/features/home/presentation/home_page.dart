e3de3b842faad301eab43093b3c82781cff2a7a2
SachaBarbet <sacha.barbet@proton.me>
2025-03-17 18:12:08 +0100

    Add speed to text with bottom sheet

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 import '../../../shared/mqtt/presentation/mqtt_config_provider.dart';
 import '../../../shared/mqtt/presentation/mqtt_service_provider.dart';
@@ -16,7 +18,12 @@ class HomePage extends ConsumerStatefulWidget {
 }
 
 class _HomePageState extends ConsumerState<HomePage> {
+  final SpeechToText _speechToText = SpeechToText();
+
   int _selectedIndex = 0;
+  bool _speechEnabled = false;
+  bool isListening = false;
+  String _lastWords = '';
 
   // List of pages to navigate to
   final List<Widget> _pages = [
@@ -28,6 +35,99 @@ class _HomePageState extends ConsumerState<HomePage> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  /// This has to happen only once per app
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening(BuildContext context) async {
+    setState(() {
+      isListening = false;
+    });
+    _speechToText.stop();
+    Navigator.pop(context);
+  }
+
+  /// This is the callback that the SpeechToText plugin calls when
+  /// the platform returns recognized words.
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      setState(() {
+        _lastWords = result.recognizedWords;
+      });
+      processCommand(_lastWords);
+    });
+  }
+
+  void _startListening(BuildContext context) async {
+    if (_speechEnabled) {
+      setState(() => isListening = true);
+      showBottomSheet(context);
+      _speechToText.listen(
+        onResult: _onSpeechResult,
+        localeId: 'fr_FR',
+      );
+    }
+  }
+
+  void processCommand(String text) {
+    text = text.toLowerCase();
+
+    if (text.contains("allume la lumière de la chambre")) {
+      print("led/chamber/control 1",);
+    } else if (text.contains("éteins la lumière de la chambre")) {
+      print("led/chamber/control 0");
+    } else if (text.contains("allume la lumière du salon")) {
+      print("led/livingroom/control 1");
+    } else if (text.contains("éteins la lumière du salon")) {
+      print("led/livingroom/control 0");
+    }
+  }
+
+  void showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isDismissible: false,
+      builder: (context) {
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return Container(
+              padding: EdgeInsets.all(16),
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    _lastWords,
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () => _stopListening(context),
+                    child: Text("Arrêter"),
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    ).whenComplete(() {
+      setState(() => isListening = false);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
   }
 
   @override
@@ -56,7 +156,18 @@ class _HomePageState extends ConsumerState<HomePage> {
 
       body: connectionState == MqttConnectionState.connected ?
         _pages[_selectedIndex] :
-        Center(child: Text('Connect to MQTT broker first')),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Center(child: Text('Connect to MQTT broker first by clicking on the settings icon in the top right corner')),
+        ),
+
+      floatingActionButton: connectionState == MqttConnectionState.connected ? FloatingActionButton(
+        onPressed: () {
+          _startListening(context);
+        },
+        tooltip: 'Listen',
+        child: Icon(isListening ? Icons.mic : Icons.mic_off),
+      ) : null,
 
       bottomNavigationBar: connectionState == MqttConnectionState.connected ?
         BottomNavigationBar(
